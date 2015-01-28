b540d7249d6b958ee17d0863decedc61349c713b
j.l <jaani.leskinen@gmail.com>
2015-01-29 01:42:00 +0200

    kokeilu

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ var fs      = require('fs');
 var app     = express();
 
 app.use('/css', express.static(__dirname+'/css'));
+app.get('/', function(req,res){res.render('index', function(err,html) {res.send(html);});
+
