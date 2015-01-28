6f468897cea234d26a021f031969df4c8292c0b5
j.l <jaani.leskinen@gmail.com>
2015-01-29 01:51:51 +0200

    vittusyntaksi

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ var app     = express();
 app.use('/css', express.static(__dirname+'/css'));
 app.get('/', function(req,res){
     res.render('index', function(err,html) {
-        res.send(html);
-        }
-    }
-    )
-    );
+        res.send(html); });
+    });
 app.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
