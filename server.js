d00fd4f6c63e2b2bf67a68882964c34b7a05b5f7
j.l <jaani.leskinen@gmail.com>
2015-01-29 01:43:52 +0200

    karsee

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ var fs      = require('fs');
 var app     = express();
 
 app.use('/css', express.static(__dirname+'/css'));
-app.get('/', function(req,res){res.render('index', function(err,html) {res.send(html);});
-
+app.get('/', function(req,res){
+    res.render('index', function(err,html) {res.send(html);
+    });
+app.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
