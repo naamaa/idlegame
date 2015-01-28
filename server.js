49ef0f9c05911e94f6331f89642593af4ecda35e
j.l <jaani.leskinen@gmail.com>
2015-01-29 01:56:05 +0200

    index.htm(l)

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app     = express();
 
 app.use('/css', express.static(__dirname+'/css'));
 app.get('/', function(req,res){
-    res.render('index', function(err,html) {
+    res.render('index.htm(l)', function(err,html) {
         res.send(html); });
     });
 app.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
