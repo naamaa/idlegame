59da5c77fea039ee2eda227f0e956e4e75c961c2
j.l <jaani.leskinen@gmail.com>
2015-01-29 02:36:47 +0200

    xxaa

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.get('/', express.static(__dirname+'/'));
 app.use('/css', express.static(__dirname+'/css'));
 app.use('/js', express.static(__dirname+'/js'));
  
-app.listen(3000 || process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
+app.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
