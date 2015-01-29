797a0c79ef1c86d2ee41b7d2bac4c1017d630b25
j.l <jaani.leskinen@gmail.com>
2015-01-29 02:27:55 +0200

    toimiva

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-#!/bin/env node
-//  OpenShift sample Node application
 var express = require('express');
 var fs      = require('fs');
 var app     = express();
-
-app.get('/', express.static(__dirname +'/index.html'));
-app.get('/', function(req,res){
-    res.render('index.html', function(err,html) {
-        res.send(html); });
-    });
-app.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
+ 
+app.get('/', express.static(__dirname+'/'));
+app.use('/css', express.static(__dirname+'/css'));
+app.use('/js', express.static(__dirname+'/js'));
+ 
+app.listen(3000 || process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
