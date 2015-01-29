73dbce8a4fb4b815de1d5383511539be116450f8
j.l <jaani.leskinen@gmail.com>
2015-01-29 02:09:01 +0200

    cx

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var express = require('express');
 var fs      = require('fs');
 var app     = express();
 
-app.use('/', express.static(__dirname +'/index.html'));
+app.get('/', express.static(__dirname +'/index.html'));
 app.get('/', function(req,res){
     res.render('index.html', function(err,html) {
         res.send(html); });
     });
 app.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
