e53d63348645a57c67ce4e82587968d5bfa7f37f
j.l <jaani.leskinen@gmail.com>
2015-01-30 17:02:07 +0200

    Server.js now creates a connection to the database

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app     = express();
+var mongourl = "mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/";
+var database = "idlegame";
  
 /*app.get('/', express.static(__dirname+'/'));
 app.use('/css', express.static(__dirname+'/css'));
@@ -10,6 +12,15 @@ app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
 app.listen(app.get('port'), process.env.OPENSHIFT_NODEJS_IP);
 // Corcon
 */ 
+
+// Creates an ongoing connection with the database. We do not close it.
+MongoClient.connect(mongourlb+database, function (err, db) {
+if (err) {
+    throw err;
+} else {
+    console.log("successfully connected to the database " + database);
+}
+
 app.use("/static", express.static(path.join(__dirname, "./html")));
 app.use('/static/css', express.static(__dirname+'/static/css'));
 app.use('/static/js', express.static(__dirname+'/static/js'));
@@ -18,3 +29,5 @@ app.use('/static/sfx', express.static(__dirname+'/static/sfx'));
 var server = app.listen(3000, function () {
 var host = server.address().address
 var port = server.address().port
+
+});
